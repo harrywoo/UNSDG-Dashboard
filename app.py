74daf0d73e76696ed26e1ce4a1c16fe9396e0dbd
Hanyu Wu <hw2753@columbia.edu>
2020-04-19 14:51:57 -0400

    Set up heroku file

--- app.py
+++ app.py
@@ -1,243 +1,204 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-from datetime import datetime
+from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
-from plotly.subplots import make_subplots
-import pymongo
-import dns
-import json
-import dash_bootstrap_components as dbc
-from yahoofinancials import YahooFinancials
-import quandl 
-import pyfolio as pf    
-import chart_studio.plotly as py    
 import plotly.graph_objs as go    
-import plotly    
-import plotly.io as pio    
-from plotly import tools
-import backtrader as bt
-from ib_insync import *
+import plotly
 import datetime
-import base64
-import io
-from PIL import Image
-import urllib
-import requests
-import matplotlib.pyplot as plt
-import random
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from os import path
-import pandas as pd
-import os
-import re
 
-# util.startLoop()
+sdg = pd.read_csv('data/data_2020.csv', index_col=0)
+sdg = sdg.reset_index()
+# sdg = pd.read_csv('data/data_rated_SDG.csv', index_col=0)
+sdg['date'] = pd.to_datetime(sdg['date'])
+# year = sdg['date'].dt.year
+# sdg = sdg[year>2019]
 
+company = sdg['COMPANY'].unique()
+MA_7day = ['MA_7day_1', 'MA_7day_2', 'MA_7day_3', 'MA_7day_4', 'MA_7day_5',
+       'MA_7day_6', 'MA_7day_7', 'MA_7day_8', 'MA_7day_9', 'MA_7day_10',
+       'MA_7day_11', 'MA_7day_12', 'MA_7day_13', 'MA_7day_14', 'MA_7day_15',
+       'MA_7day_16', 'MA_7day_17']
+MA_60day = ['MA_60day_1', 'MA_60day_2',
+       'MA_60day_3', 'MA_60day_4', 'MA_60day_5', 'MA_60day_6', 'MA_60day_7',
+       'MA_60day_8', 'MA_60day_9', 'MA_60day_10', 'MA_60day_11', 'MA_60day_12',
+       'MA_60day_13', 'MA_60day_14', 'MA_60day_15', 'MA_60day_16',
+       'MA_60day_17']
+SDG_std = ['SDG_1_std', 'SDG_2_std',
+       'SDG_3_std', 'SDG_4_std', 'SDG_5_std', 'SDG_6_std', 'SDG_7_std',
+       'SDG_8_std', 'SDG_9_std', 'SDG_10_std', 'SDG_11_std', 'SDG_12_std',
+       'SDG_13_std', 'SDG_14_std', 'SDG_15_std', 'SDG_16_std', 'SDG_17_std']
+SDG_count = ['SDG_1_count', 'SDG_2_count', 'SDG_3_count',
+       'SDG_4_count', 'SDG_5_count', 'SDG_6_count', 'SDG_7_count',
+       'SDG_8_count', 'SDG_9_count', 'SDG_10_count', 'SDG_11_count',
+       'SDG_12_count', 'SDG_13_count', 'SDG_14_count', 'SDG_15_count',
+       'SDG_16_count', 'SDG_17_count']
 
-df_theme = pd.read_csv('UNGC_SDG_antonyms - March12.csv', encoding = 'utf-8')
-feature = df_theme.columns.values.tolist()
-feature.remove('Unnamed: 0')
-df_theme = df_theme.dropna()
+MA_type = ['MA_7day','MA_60day']
 
-# set the color of the wordcloud gray-ish to make the entire dash harmonic
-def grey_color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+sdg_type = ['SDG_1', 'SDG_2', 'SDG_3', 'SDG_4', 'SDG_5', 'SDG_6', 'SDG_7', 'SDG_8', 'SDG_9',
+            'SDG_10', 'SDG_11', 'SDG_12', 'SDG_13', 'SDG_14', 'SDG_15', 'SDG_16', 'SDG_17']
 
-def encode_image(image_file):
-    encoded = base64.b64encode(open(image_file, 'rb').read())
-    return 'data:image/png;base64,{}'.format(encoded.decode())
+data_type = ["SDG", "MA", "STD", "COUNTS"]
+data_type_dict = {"SDG":sdg_type, "MA":MA_60day, "STD":SDG_std, "COUNTS":SDG_count}
 
+first_day = sdg.groupby("Ticker").first()["date"].value_counts().index[0]
+last_day = sdg.groupby("Ticker").last()["date"].value_counts().index[0]
 
+start_date = last_day - datetime.timedelta(days=30)
+end_date = last_day
+date_list = sdg[sdg["Ticker"]==sdg.loc[0]["Ticker"]]["date"]
+date_dict = date_list.to_dict()
+date_str_dict = date_list.dt.date.map(str).to_dict()
+date_length = len(date_dict)-1
 
-filePath = r"SDG outliers_specific (no rank)_results"
-files = os.listdir(filePath)
-# files.remove('.DS_Store')
+for index, value in date_str_dict.items():
+    if index not in [0, int(date_length/2), date_length]:
+        date_str_dict[index] = ""
+# date_str_dict
 
-SDG_info = []
-update_SDG_info = []
-company = []
-date = []
-SDG = []
-for i in files:
-    path = 'SDG outliers_specific (no rank)_results/' + i
-    if os.path.isdir(path):
-        inf = os.listdir(path)
-        SDG_info.extend(inf)
-for i  in SDG_info:
-    if 'pdf' in i:
-        update_SDG_info.append(i)
-pattern = r'^(.*)\s+\((\d+.*)\).*(\d{4}-\d{2}-\d{2})\.\w+'
-for string in update_SDG_info:
-    info = re.findall(pattern, string)
-    company.append(info[0][0])
-    date.append(info[0][2])
-    SDG.append(info[0][1])
-
-out_result = pd.DataFrame({'SDG_number':SDG,
-                         'Company_name':company,
-                         'Date':date})
-
-date = sorted(set(date))
-company = sorted(set(company))
-# set color for different parts
 colors = {'background': '#111111', 'text': '#7FDBFF','button':'#FFFF00'}
-
 app = dash.Dash()
-server = app.server
-#app.css.append_css({'remove'})
-app.layout = html.Div(
-    children = [
-        html.H1(children = 'Wordcloud of SDG Keywords', style = {'color' : colors['text'],
-                                                                 'width' : '50%', 
-                                                                 'display': 'inline-block',
-                                                                 'margin-top': 10,
-                                                                 'margin-left': 10}),
-        html.Img(
+
+app.layout = html.Div([
+        html.Br(),
+        html.Br(),
+        # header and logo
+        html.Div([
+            html.H1('SDG Time Series', className = 'ten columns', style = {'margin-top': 10,'margin-left': 15, 'color': colors['text']}),
+
+            html.Img(
                 src = 'https://images.squarespace-cdn.com/content/5c036cd54eddec1d4ff1c1eb/1557908564936-YSBRPFCGYV2CE43OHI7F/GlobalAI_logo.jpg?content-type=image%2Fpng',
                 style = {
-                    'height': '4.4%',
+                    'height': '11%',
                     'width': '11%',
                     'float': 'right',
-                    'margin-top': 20,
-                    'margin-right': 20,
-                    'display': 'inline-block' 
-                }),
+                    'position': 'relative',
+                    'margin-top': 11,
+                    'margin-right': 0
+                },
+                className = 'two columns'        
+            )  
+        ], className = 'row'),
+
         html.Br(),
-        html.Div([
-            html.H6('Select an SDG to visualize its keywords', 
-                    style = {'color': '#9999FF'}),
-            # dropdown
-            dcc.Dropdown(
-                id = 'SDGnumber',
-                options = [{'label':i, 'value':i} for i in feature],
-                # value = '1. No Poverty' # next row sets the initial value showed in the dropdown box null
-                searchable = False,
-                # set the initial text
-                placeholder = "Please select a feature"
-                # set height
-                #optionHeight = 40
-            )
-        ], style = {"width": "30%",'margin-left': 10,'display': 'inline-block'}),
-         html.Div([
-            html.H6('Also select a company name please', 
-                    style = {'color': '#9999FF'}),
-            # dropdown
-            dcc.Dropdown(
-                id = 'company_name',
-                options = [{'label':i, 'value':i} for i in company],
-                # value = '1. No Poverty' # next row sets the initial value showed in the dropdown box null
-                searchable = False,
-                # set the initial text
-                placeholder = "Please select a feature"
-                # set height
-                #optionHeight = 40
-            )
-        ], style = {"width": "30%",'margin-left': 10,'display': 'inline-block'}),
-        html.Div([
-            html.H6('Also select a date please', 
-                    style = {'color': '#9999FF'}),
-            # dropdown
-            dcc.Dropdown(
-                id = 'date',
-                options = [{'label':i, 'value':i} for i in date],
-                # value = '1. No Poverty' # next row sets the initial value showed in the dropdown box null
-                searchable = False,
-                # set the initial text
-                placeholder = "Please select a feature"
-                # set height
-                #optionHeight = 40
-            )
-        ], style = {"width": "30%",'margin-left': 10,'display': 'inline-block'}),
+        html.Br(),
+        html.Br(),
         
+    
+        # select the company 
+        html.Div([
+            html.H3('Select the company:', style={'paddingRight':'30px','color': '#9999FF'}),
+            dcc.Dropdown(
+            id = 'company',
+            options = [{'label':i, 'value':i} for i in company],
+            value = 'apple inc'
+            )
+        ], style={"width": "25%"}),
+        
+        # select the type of MA you want to see
+        html.Div([
+            html.H3('Select the data type:', style={'paddingRight':'30px','color': '#9999FF'}),
+            dcc.Dropdown(
+            id = 'data_type',
+            options = [{'label':i, 'value':i} for i in data_type],
+            value = data_type[0]
+            )
+        ], style={"width": "25%"}),
+        
+        # select time range of MA time series
+        html.Div([
+            html.H3('Select the time zone:', style={'paddingRight':'30px','color': '#9999FF'}),
+            dcc.Slider(id="current_date", min=0, max=date_length, value=date_length,
+                                 marks=date_str_dict)],style={"width": "50%"}),
+    
+        # Scatter chart
+        html.Div([
+             html.H3('Time series of MA_sdg you selected', style={'paddingRight':'30px','color': colors['text']}),
+        html.Div([
+                dcc.Graph(id = 'scatter')
+            ], className = 'twelve columns'),
+        ], className = 'row',style={"height" : '50vh', "width" : "70%",'margin-left': 0, 
+                                    'margin-right': 0,'margin-top':0,'margin-bottom':0}),
+        html.Br(), 
+        # Histogram chart
+        html.Div([
+             html.H3('Distribution of SDG you selected', style={'paddingRight':'30px','color': colors['text']}),
+        html.Div([
+                dcc.Graph(id = 'histogram')
+            ], className = 'twelve columns'),
+        ], className = 'row',style={"height" : '50vh', "width" : "70%",'margin-left': 0, 
+                                    'margin-right': 0,'margin-top':0,'margin-bottom':0}),
         html.Br(),
+        # Heatmap
+        html.Div([
+             html.H3('Distribution of SDG you selected', style={'paddingRight':'30px','color': colors['text']}),
+        html.Div([
+                dcc.Graph(id = 'heatmap')
+            ], className = 'twelve columns'),
+        ], className = 'row',style={"height" : '50vh', "width" : "70%",'margin-left': 0, 
+                                    'margin-right': 0,'margin-top':0,'margin-bottom':0}),
         html.Br(),
-        html.Br(),
-        # graph
-        html.Div(id="graph-container", children=[
-            html.H6('Visualized Results:', style={'color': colors['text'],
-                                                  'margin-left': 10,
-                                                  'width': '30%'}),
-            html.Div(html.Img(id='Graph', style={}), style={
-                'margin-left': 300, 'margin-right': 10}),
-            html.Div(dcc.Graph(id='urlTable')),
-            html.Div(dcc.Graph(id='timeTable'))], style={'display':'block'})
-    ], style={'backgroundColor': colors['background'], 'height': '100vh'})
-image_dir = 'images/'
-csv_dir = 'SDG outliers_specific (no rank)_results/'
+
+],style = {'backgroundColor': colors['background']}) 
+        
+
 
 
 @app.callback(
-    Output('graph-container', 'style'),
-    [Input('SDGnumber', 'value'), Input('company_name', 'value'), Input('date', 'value')])
-def hide_graph(SDG_number, company_name, date):
-    filename = image_dir+f'{SDG_number}_{company_name}_{date}.png'
-    if os.path.exists(filename):
-        return {'display': 'block'}
-    else:
-        return {'display': 'none'}
+    Output('scatter', 'figure'),
+    [Input('company', 'value'), Input('data_type', 'value'), Input('current_date', 'value')])
+def update_figure(company, data_type, current_date):
+    df = sdg[sdg['COMPANY'] == company]
 
+    current_date = date_dict[current_date]
+#     current_date = last_day
+    start_date = current_date - datetime.timedelta(days=30)
 
-@app.callback(
-    Output('Graph', 'src'),
-    [Input('SDGnumber', 'value'), Input('company_name', 'value'), Input('date', 'value')])
-def update_graph(SDG_number, company_name, date):
-    return encode_image(image_dir+f'{SDG_number}_{company_name}_{date}.png')
-
-
-@app.callback(
-    Output('urlTable', 'figure'),
-    [Input('SDGnumber', 'value'), Input('company_name', 'value'), Input('date', 'value')])
-def update_table(SDG_number, company_name, date):
-    SDG_number = re.findall("\d+", SDG_number)[0]
-    SDG_dir = csv_dir + "SDG"+str(SDG_number)+" result(unranked)/"
-    file_names = os.listdir(SDG_dir)
-    p_url = re.compile(company_name+r'\ all\ url.+\.csv')
-    url_filename = [s for s in file_names if p_url.match(s)][0]
-    url_df = pd.read_csv(SDG_dir+url_filename)
-    data = go.Table(
-        header=dict(values=['Link'],
-                    line_color='darkslategray',
-                    fill_color='lightskyblue',
-                    align='left'),
-        cells=dict(values=url_df.T.iloc[1:],  # 2nd column
-                   line_color='darkslategray',
-                   fill_color='lightcyan',
-                   align='left'))
-    layout = go.Layout(plot_bgcolor=colors['background'],
-                       paper_bgcolor=colors['background'], title=dict(text='Url Link',
-                                                                      font=dict(color='white')), height=600)
-    return {"data": [data], "layout": layout}
-
+    df = df[(df["date"]>=start_date) & (df["date"]<=current_date)]
+    
+    trace = [go.Scatter(x=df["date"], y=df[x], mode='lines',
+                            marker={'size': 8, "opacity": 0.6, "line": {'width': 0.5}}, ) for x in data_type_dict[data_type]]
+    return {"data": trace,
+            "layout": go.Layout(plot_bgcolor = colors['background'],
+                  paper_bgcolor = colors['background'],font = {'color': colors['text']},title="Daily line chart of SDG", colorway=['#fdae61', '#abd9e9'],
+                                yaxis={"title": "Rate"}, xaxis={"title": "Date"})}
 
 @app.callback(
-    Output('timeTable', 'figure'),
-    [Input('SDGnumber', 'value'), Input('company_name', 'value'), Input('date', 'value')])
-def update_table(SDG_number, company_name, date):
-    SDG_number = re.findall("\d+", SDG_number)[0]
-    SDG_dir = csv_dir + "SDG"+str(SDG_number)+" result(unranked)/"
-    file_names = os.listdir(SDG_dir)
-    p_time = re.compile(company_name+r'\ time.+\.csv')
+    Output('histogram', 'figure'),
+    [Input('company', 'value'), Input('data_type', 'value'), Input('current_date', 'value')])
+def update_figure(company, data_type, current_date):
+    df = sdg[sdg['COMPANY'] == company]
+    current_date = date_dict[current_date]
 
-    time_filename = [s for s in file_names if p_time.match(s)][0]
-    time_df = pd.read_csv(SDG_dir+time_filename)
-    data = go.Table(
-        header=dict(values=['Index', 'Company', 'Date', 'SDG'],
-                    line_color='darkslategray',
-                    fill_color='lightskyblue',
-                    align='left'),
-        cells=dict(values=time_df.T,  # 2nd column
-                   line_color='darkslategray',
-                   fill_color='lightcyan',
-                   align='left'))
-    layout = go.Layout(plot_bgcolor=colors['background'],
-                       paper_bgcolor=colors['background'], title=dict(text='Time Series',
-                                                                      font=dict(color='white')), height=600)
-    return {"data": [data], "layout": layout}
+#     current_date = last_day
+    start_date = current_date - datetime.timedelta(days=30)
 
+    df = df[(df["date"]>=start_date) & (df["date"]<=current_date)]
+    
+    trace = [go.Histogram(x=df[x]) for x in data_type_dict[data_type]]
+    return {"data": trace,
+            "layout": go.Layout(plot_bgcolor = colors['background'],
+                  paper_bgcolor = colors['background'],font = {'color': colors['text']},title="Daily histogram of SDG", colorway=['#fdae61', '#abd9e9'],
+                                yaxis={"title": "Rate"}, xaxis={"title": "Date"})}
+
+@app.callback(
+    Output('heatmap', 'figure'),
+    [Input('company', 'value'), Input('data_type', 'value'), Input('current_date', 'value')])
+def update_figure(company, data_type, current_date):
+    df = sdg[sdg['COMPANY'] == company]
+    
+    current_date = date_dict[current_date]
+#     current_date = last_day
+    date = current_date - datetime.timedelta(days=1)
+    df = df[df["date"]==date]
+    
+    trace = [go.Heatmap(z=df[sdg_type])]
+    return {"data": trace,
+            "layout": go.Layout(plot_bgcolor = colors['background'],
+                  paper_bgcolor = colors['background'],font = {'color': colors['text']},title="Daily heatmap of SDG", colorway=['#fdae61', '#abd9e9'], 
+                                yaxis={"title": "Rate"}, xaxis={"title": "Date"})}
 
 if __name__ == '__main__':
     app.run_server()
